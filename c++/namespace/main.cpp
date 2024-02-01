b28ec59dc7d148b961be955648afae93de745977
komaladeviai01 <komaladeviai01@gmail.com>
2024-02-01 13:03:48 +0530

    added main.cpp

---
+++ c++/namespace/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  /* string name;
+   cout<<"enter your string name:"<<endl;
+   cin>>name;
+   cout<<"enter your name:"<<name<<endl;
+   fflush(stdin);
+   cout<<"enter your class name:"<<endl;
+   getline(cin,name);
+   cout<<name<<endl;*/
+
+   /*string firstname;
+   string lastname;
+   cin>>firstname>>lastname;
+   cout<<firstname+" "+lastname<<endl;
+   string fullname=firstname.append(lastname);
+   cout<<fullname<<endl;
+*/
+  string student;
+   cin>>student;
+   student.push_back('s');
+   cout<<student<<endl;
+   return 0;
+
+}
