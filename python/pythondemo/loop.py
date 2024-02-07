d50e6d9ab080c88825f0c31a1cd3b5b0dbf308aa
Komaladevi S <komaladeviai01@brightskills.com>
2024-02-07 13:27:10 +0530

    added loop.py file

--- python/pythondemo/loop.py
+++ python/pythondemo/loop.py
@@ -1,10 +1,17 @@
 i=0
-while i<9:
+while i<5:
     i+=1
     if i==3:
         continue
     print("enter the value",i)
     
-   
+x=30
+if x>10:
+    print("If statement")
+    if x<15:
+        print("greater than 15:")
+    else:
+        print("less than value" )
+
 
     
