1c4e51c2e17722c11aa5ba9be4b341b8e1756abb
Komaladevi S <komaladeviai01@brightskills.com>
2024-02-13 13:37:03 +0530

    added Inheritance.py file

--- python/pythondemo/Inheritance.py
+++ python/pythondemo/Inheritance.py
@@ -1,23 +1,41 @@
-class parent():
+class company():
     def __init__(self):
-        print("App access to your information")
+        print("The company details")
 
-    def AccessChrome(self):
-        print("login ")
-    def Name(self):
-        print("Enter the name")
-    def Email(self):
-        print("Enter the email" )
-    def OTP(self):
-        print("would you enter the OTP")
-class child(parent):
+    def companyname(self):
+        print(" The company name SBK ")
+    def accessnetwork (self):
+        print("The company provite network")
+    def Id (self):
+        print("The id corde access" )
+    def password(self):
+        print("Allow to changed password of system")
+    def shareholder(self):
+        print ("The control of company")
+class manager(company):
+    def __init__(self):
+        super(). companyname()
+        super().accessnetwork()
+        super().Id()
+        super().password()
+    def ordertostaff(self):
+        print(" Distributed the work properly ")
+    def communicate(self):
+        print("The staff proplem or company work  ")
+              
+class customer(company):
     def __init__(self):
         super().__init__()
-        super().AccessChrome()
-        super().Name()
-        super().Email()
-        super().OTP()
-    
+        super().companyname()
+    def feelpack(self):
+        print("The how it is work ")
+    def regiment(self):
+        print("Thery are regiment this company")
+
+
+x=company()
+y=manager()
+z=customer()
 
         
         
