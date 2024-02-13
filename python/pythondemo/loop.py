f841447f1622b60b0df9c3de11498f7cdd812db8
Komaladevi S <komaladeviai01@brightskills.com>
2024-02-13 11:54:11 +0530

    added loop.py file

--- python/pythondemo/loop.py
+++ python/pythondemo/loop.py
@@ -1,17 +1,28 @@
-i=0
+"""i=0
 while i<5:
     i+=1
     if i==3:
         continue
     print("enter the value",i)
-    
+#if statement   
 x=30
 if x>10:
     print("If statement")
     if x<15:
         print("greater than 15:")
     else:
-        print("less than value" )
+        print("less than value" )"""
 
+#for loop
+name=["kalai","selvi","ravi"]
+for x in name:
+        print(x)
+        if x=="selvi":
+            break
 
-    
+place=["pudukkottai","trichy","madurai"]
+for x1 in place:
+        if x1=='trichy':
+            break
+        print(x1)
+        
