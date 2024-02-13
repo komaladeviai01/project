c52716e48ddb58799866a195a70ede16e7ea9b5e
Komaladevi S <komaladeviai01@brightskills.com>
2024-02-13 11:52:38 +0530

    added pythonmysql.py file

---
+++ python/pythondemo/pythonmysql.py
@@ -0,0 +1,18 @@
+import mysql.connector
+con=mysql.connector.connect(
+
+    host="192.168.1.240",
+    user="AIBATCH01",
+    password="AI@123",
+    database="a"
+)
+
+
+print(con)
+result=con.cursor()
+result.execute("show tables")
+resultshow=result.fetchall()
+for x in resultshow:
+    print(x)
+
+
