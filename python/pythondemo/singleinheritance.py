5f1d9666eb33928b61f6578b9ae88a82af27ffea
Komaladevi S <komaladeviai01@brightskills.com>
2024-02-14 11:23:24 +0530

    added singleinheritance.py file

---
+++ python/pythondemo/singleinheritance.py
@@ -0,0 +1,21 @@
+class company():
+    def __init__(self):
+        print("The company details")
+
+    def companyname(self):
+        print(" The company name SBK ")
+    def accessnetwork (self):
+        print("The company provite network")
+
+class manager(company):
+    def __init__(self):
+        super().__init__()
+        super().companyname()
+        super().accessnetwork()
+        print("The company details")
+        
+    def ordertostaff(self):
+        print(" Distributed the work properly ")
+              
+a=manager()
+a.ordertostaff()
