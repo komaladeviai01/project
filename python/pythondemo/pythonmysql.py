61b62601ec24aec1fbf363816d834aba4ba9a03e
Komaladevi S <komaladeviai01@brightskills.com>
2024-02-17 11:08:32 +0530

    added pythonmysql.py file

--- python/pythondemo/pythonmysql.py
+++ python/pythondemo/pythonmysql.py
@@ -4,15 +4,36 @@ con=mysql.connector.connect(
     host="192.168.1.240",
     user="AIBATCH01",
     password="AI@123",
-    database="a"
+    database="ai_komathi"
 )
 
 
 print(con)
 result=con.cursor()
 result.execute("show tables")
-resultshow=result.fetchall()
-for x in resultshow:
+result_show=result.fetchall()
+for x in result_show:
     print(x)
+
+result.execute("select * from std_info")
+result_show=result.fetchall()
+for x in result_show:
+    print(x)
+
+
+
+result.execute
+result_show=result.fetchall()
+for x in result_show:
+   print(x)
+result.execute("select * from std_info")
+result_show=result.fetchall()
+for x in result_show:
+    print(x)
+#result.execute("describe purchase")
+#result_show=result.fetchall()
+#for x in result_show :
+ #   print(x)
+
 
 
